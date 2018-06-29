287ded511c9d0acba1a448a70d21e6d6040c72fc
bredele <olivier.wietrich@gmail.com>
2018-06-29 09:46:39 -0600

    Remove booleans from concatenated value

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ module.exports = function () {
   var classes = ''
   for (var i = 0, l = arguments.length; i < l; i++) {
     var item = arguments[i]
-    if (item) classes += item + ' '
+    var type = typeof item
+    if (item && type !== 'boolean') classes += item + ' '
   }
   return classes.trim()
 }
--- test/spruceup.js
+++ test/spruceup.js
@@ -14,3 +14,8 @@ test('should only concatenate truthy values', assert => {
   assert.plan(1)
   assert.equal(classes('hello', null, false, 'world'), 'hello world')
 })
+
+test('should not concatenate values with truthy boolean', assert => {
+  assert.plan(1)
+  assert.equal(classes('hello', true, 'world'), 'hello world')
+})
