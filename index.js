7743ce5af4b411f588db7670dfa100b1dcd66c9d
bredele <olivier.wietrich@gmail.com>
2018-06-29 09:42:17 -0600

    Refactor code and trim result

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = function () {
   var classes = ''
   for (var i = 0, l = arguments.length; i < l; i++) {
-    classes += arguments[i] + (i !== (l - 1) ? ' ' : '')
+    classes += arguments[i] + ' '
   }
-  return classes
+  return classes.trim()
 }
